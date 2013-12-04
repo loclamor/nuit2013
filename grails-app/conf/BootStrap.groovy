0a922ff1a6e6f12437b7467a87e9b55960bc8fcb
Kevin ANATOLE <kevinanatole@yahoo.fr>
2013-12-04 23:17:20 +0100

    Modification du bootstrap pour coller à la nouvelle version de User

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,10 +8,10 @@ class BootStrap {
         def userRole = SecRole.findByAuthority('ROLE_USER')?:new SecRole(authority: 'ROLE_USER').save()
         def adminRole = SecRole.findByAuthority('ROLE_ADMIN')?:new SecRole(authority: 'ROLE_ADMIN').save()
 
-        def users = SecUser.list()?:[]
-        if(!users){
-            def standardUser = new SecUser(username: "stdUser", password: "stdPassword").save()
-            def adminUser = new SecUser(username: "admin", password: "adminPassword").save()
+        def users = SecUser.list()
+        if(!users.size() == 0){
+            def standardUser = new SecUser(username: "stdUser", password: "stdPassword", email: "kevinanatole@yopmail.com").save()
+            def adminUser = new SecUser(username: "admin", password: "adminPassword", email: "kevinanatole@yahoo.fr").save()
         }
     }
     def destroy = {
