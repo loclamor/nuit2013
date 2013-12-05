77f3972a52d8255fc69c742d4e538ae269df7c5f
Florian CLAVEL <florian.clavel@live.Fr>
2013-12-05 23:09:28 +0100

    Add exhange class

---
+++ grails-app/domain/nuitinfo2013/Exchange.groovy
@@ -0,0 +1,12 @@
+package nuitinfo2013
+
+class Exchange {
+
+	def exchangeValidate(User userA, User userB, Product productA, Product productB) {
+		
+	}
+	
+	def exchangeDenied(User userA, User userB, Product productA, Product productB) {
+		
+	}
+}
