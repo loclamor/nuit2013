64cd074dd9548c65eaf53f5e9c06d3bb8a8643dc
Jeremy BOURDIOL <jerem.dante@gmail.com>
2013-12-05 23:42:36 +0100

    debut des exchange

--- grails-app/domain/nuitinfo2013/Exchange.groovy
+++ grails-app/domain/nuitinfo2013/Exchange.groovy
@@ -1,5 +1,7 @@
 package nuitinfo2013
 
+import com.sun.jmx.snmp.UserAcl;
+
 class Exchange {
 
 	def exchangeValidate(User userA, User userB, Product productA, Product productB) {
@@ -7,6 +9,20 @@ class Exchange {
 	}
 	
 	def exchangeDenied(User userA, User userB, Product productA, Product productB) {
+		/*Preference prefA = findByUserAndProduct(userA,productA);
+		Preference prefB = findByUserAndProduct(userB,productB);
+		if(userA.exchange){
+			prefA.score++
+		}else{
+			prefA.score--
+		}
+		if(userB.exchange){
+			prefB.score++
+		}else{
+			prefB.score--
+		}*/
+		
+		
 		
 	}
 }
