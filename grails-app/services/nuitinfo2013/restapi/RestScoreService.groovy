f2b45058f33fe55f88ae1d5aa963760a7ac1b70a
Simar Jeremy <exasky@live.fr>
2013-12-06 05:02:40 +0100

    -

--- grails-app/services/nuitinfo2013/restapi/RestScoreService.groovy
+++ grails-app/services/nuitinfo2013/restapi/RestScoreService.groovy
@@ -5,7 +5,7 @@ import nuitinfo2013.Rating
 
 class RestScoreService {
 
-    def temp(String productName) {
+    def buildRequestRepresentation(String productName) {
         Map representation
 
         if (!productName) {
@@ -36,19 +36,4 @@ class RestScoreService {
 
         return representation
     }
-
-    /*def findScoreForProduct(String productLibelle) {
-        Integer score = 0
-
-        if (!productLibelle) {
-            throw new IllegalArgumentException()
-        }
-
-        Product product = Product.findByName(productLibelle)
-
-        def preferenceList = Rating.findAllByProduct(product)
-        preferenceList.each { score += it.elo }
-
-        return score
-    }*/
 }
