e171c450884606ad2459c67c80e7be54d42e06af
Vincent Tertre <vincent.tertre@ticetime.com>
2013-12-06 01:30:25 +0100

    API Score, get score for given product name

--- grails-app/controllers/nuitinfo2013/restapi/v1/RestProductController.groovy
+++ grails-app/controllers/nuitinfo2013/restapi/v1/RestProductController.groovy
@@ -1,20 +1,11 @@
 package nuitinfo2013.restapi.v1
 
-import grails.converters.JSON
-import nuitinfo2013.Product
-import nuitinfo2013.restapi.RestProductService
 
 class RestProductController {
 
     static allowedMethods = [retrieve: 'GET']
 
-    RestProductService restProductService
+    def retrieve(String format) {
 
-    def retrieve(String product_id, String format) {
-        Integer score = restProductService.findScoreForProduct(product_id)
-
-        // TODO handle format
-
-        render([score: score] as JSON)
     }
 }
--- grails-app/services/nuitinfo2013/restapi/RestProductService.groovy
+++ grails-app/services/nuitinfo2013/restapi/RestProductService.groovy
@@ -1,24 +1,6 @@
 package nuitinfo2013.restapi
 
-import nuitinfo2013.Preference
-import nuitinfo2013.Product
 
 class RestProductService {
 
-    def findScoreForProduct(String productLibelle) {
-        Integer score
-
-        if (!productLibelle) {
-            throw new IllegalArgumentException()
-        }
-
-        Product product = Product.findByLibelle(productLibelle)
-        def preferenceList = Preference.findAllByProduct(product)
-        /*def criteria = Preference.createCriteria()
-        def results = criteria.list {
-            eq 'product.libelle', product.libelle
-        }*/
-        preferenceList.each { score += it.score }
-        return score
-    }
 }
